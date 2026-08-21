1bfc0c9450de08b3829d067c5051b5a55d2ea61e
ialria <your_real_email@gmail.com>
2026-08-21 14:28:25 +0500

    Resolved all comments and modified authRoute

--- backend/src/routes/authRoute.js
+++ backend/src/routes/authRoute.js
@@ -1,12 +1,12 @@
 const express=require("express");
-const {loginLimiter,forgotPasswordLimiter,resetPasswordLimiter,signupLimiter} = require("../middleware/rateLimiter");
+const {authLimiter} = require("../middleware/rateLimiter");
 const {signup, login, logout, forgotPassword, resetPassword}=require("../controllers/authController");
 const authLimiter = require("../middleware/authLimiter");
 const router=express.Router();
-router.post("/signup",signupLimiter,signup);
-router.post("/login",loginLimiter,login);
+router.post("/signup",signup);
+router.post("/login",authLimiter,login);
 router.post("/logout",logout);
-router.post("/forgot-password", forgotPasswordLimiter,forgotPassword);
-router.post("/reset-password", resetPasswordLimiter,resetPassword);
+router.post("/forgot-password", authLimiter,forgotPassword);
+router.post("/reset-password", authLimiter,resetPassword);
 
 module.exports=router;
